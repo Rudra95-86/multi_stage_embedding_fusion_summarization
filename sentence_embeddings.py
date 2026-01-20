6056082bfd3a367d08ea791d416ea531cbf1f188
Rudra mistri <rudramistri2004@gmail.com>
2026-01-20 17:01:23 +0530

    sentence_embeddings.py

--- sentence_embeddings.py
+++ sentence_embeddings.py
@@ -3,6 +3,7 @@ from sentence_transformers import SentenceTransformer
 # Load model once
 model = SentenceTransformer("all-MiniLM-L6-v2")
 
+@st.cache_resource
 def get_sentence_bert_embeddings(sentences):
     """
     Returns real sentence embeddings
@@ -14,3 +15,4 @@ def get_sentence_bert_embeddings(sentences):
         normalize_embeddings=False
     )
     return embeddings
+
